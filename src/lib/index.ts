11b7f6c0e71fcfe0d6ffce428634ff7bdb11dc07
Soul Codes <reach.soul.codes@gmail.com>
2023-02-03 18:38:31 +0700

    fix: 🐛 missing async export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,4 @@ export { DecodeError, draw, failure, success } from "io-ts/lib/Decoder.js";
 export * from "./apply.js";
 export * from "./types.js";
 export * from "./factories.js";
+export * from "./async.js";
